a9bf1db059f813cab5bb60eccdf3a9b319b9cc9a
Camryn Scott <camryn1242@yahoo.com>
2023-06-01 22:00:08 -0400

    finished adding questions and options to question routes

--- app.py
+++ app.py
@@ -15,16 +15,25 @@ def show_home_page():
 
 @app.route('/questions/0')
 def show_question1():
-    return render_template('question1.html')
+    question = survey.questions[0]
+    choices = question.choices
+    print(choices)
+    return render_template('question1.html',survey=survey,question=question,choices=choices)
 
 @app.route('/questions/1')
-def show_question1():
-    return render_template('question2.html')
+def show_question2():
+    question = survey.questions[1]
+    choices = question.choices
+    return render_template('question2.html',survey=survey,question=question,choices=choices)
 
 @app.route('/questions/2')
-def show_question1():
-    return render_template('question3.html')
+def show_question3():
+    question = survey.questions[2]
+    choices = question.choices
+    return render_template('question3.html',survey=survey,question=question,choices=choices)
 
 @app.route('/questions/3')
-def show_question1():
-    return render_template('question4.html')
+def show_question4():
+    question = survey.questions[3]
+    choices = question.choices
+    return render_template('question4.html',survey=survey,question=question,choices=choices)
